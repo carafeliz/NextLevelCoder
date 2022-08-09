4e0350c4631bd3fb9a13188f75282c4da459aa57
carafeliz <nastelala@gmail.com>
2022-08-08 20:38:05 -0400

    Class 0

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,17 @@
+class Dinosaur:
+    def __init__ (self):
+        pass
+
+    def update (self):
+        pass   
+
+    def run (self):
+        pass
+
+    def duck (self):
+        pass
+    def jump (self):
+        pass
+
+    def draw (self):
+        pass
