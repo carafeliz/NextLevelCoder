90be50a8198eae8d170dde666a84e06dc4f265cf
carafeliz <nastelala@gmail.com>
2022-08-10 19:28:27 -0400

    Class 2:  Obstacles/Small cactus

---
+++ dino_runner/components/obstacles/obstacle.py
@@ -0,0 +1,17 @@
+from dino_runner.utils.constants import SCREEN_HEIGHT
+from pygame.sprite import Sprite
+
+class Obstacle(Sprite):
+    def __init__(self, image, type):
+        self.image = image
+        self.type = type
+        self.rect = self.image[self.type].get_rect() #rect devuelve la pantalla y para usarlo se importa pygame
+        self.rect.x = SCREEN_HEIGHT
+
+    def update(self, game_speed, obstacles):
+        self.rect.x -= game_speed
+        if self.rect.x < -self.rect.width:
+            obstacles.pop()
+
+    def draw (self, screen):
+        screen.blit(self.image[self.type], self.rect)
